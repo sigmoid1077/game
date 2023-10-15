8ad8ef9ba948c6179aa7cb4e5ff12f74cd4edc5a
sigmoid1077 <victorascarano@gmail.com>
2023-10-15 17:14:37 -0500

    Update

---
+++ util/src/net/mod.rs
@@ -0,0 +1,53 @@
+use std::net::{TcpListener, TcpStream};
+use serde::{Serialize, Deserialize};
+use bevy::ecs::component::Component;
+
+pub mod client;
+pub mod server;
+
+pub use client::{
+    ClientPlugin,
+    ConnectEvent,
+    ServerUnboundEvent,
+    DisconnectEvent,
+    SendPacketToServerEvent,
+    RecievedPacketFromServerEvent,
+};
+
+pub use server::{
+    ServerPlugin,
+    BindEvent,
+    UnbindEvent,
+    ClientConnectedEvent,
+    ClientDisconnectedEvent,
+    SendPacketToClientEvent,
+    SendPacketToAllClientsEvent,
+    RecievedPacketFromClientEvent
+};
+
+#[derive(Component)]
+struct TcpListenerComponent {
+    pub tcp_listener: TcpListener
+}
+
+impl TcpListenerComponent {
+    fn new(tcp_listener: TcpListener) -> Self {
+        Self { tcp_listener }
+    }
+}
+
+#[derive(Component)]
+struct TcpStreamComponent {
+    pub tcp_stream: TcpStream
+}
+
+impl TcpStreamComponent {
+    fn new(tcp_stream: TcpStream) -> Self {
+        Self { tcp_stream }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum Packet {
+    MyPacket(String)
+}
